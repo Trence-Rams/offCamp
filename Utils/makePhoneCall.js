080ecf0db1958a1243ed20b6dab98c3f6f1d5232
Terrence <trencerams@gmail.com>
2024-10-29 00:19:24 +0200

    refining the code

--- Utils/makePhoneCall.js
+++
@@ -1,9 +0,0 @@
-import * as Linking from "expo-linking";
-
-export const makePhoneCall = (phoneNumber) => {
-  if (!phoneNumber) throw new Error("Phone number is required");
-
-  Linking.openURL(`tel:${phoneNumber}`).catch((err) => {
-    console.error("Failed to open phone dialer:", err);
-  });
-};
