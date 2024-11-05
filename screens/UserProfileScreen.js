83dd29ecb8992c1ba7b84b09e54af308ed0aa784
Terrence <trencerams@gmail.com>
2024-11-05 15:17:29 +0200

    added a signUpScreen

--- screens/UserProfileScreen.js
+++ screens/UserProfileScreen.js
@@ -2,18 +2,19 @@ import React from "react";
 import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
 import { Icon } from "react-native-elements";
 import SettingsComponent from "../components/SettingsComponent";
-const PersonalInfoScreen = () => {
+import UserProfileScreenStyles from "../styles/UserProfileScreenStyles";
+const UserProfileScreen = () => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.header}>profile</Text>
-      <View style={styles.photoContainer}>
+    <View style={UserProfileScreenStyles.container}>
+      <Text style={UserProfileScreenStyles.header}>profile</Text>
+      <View style={UserProfileScreenStyles.photoContainer}>
         <Icon
           name="account-circle"
           type="material-community"
           size={80}
           color="#d3d3d3"
         />
-        <TouchableOpacity style={styles.addPhotoButton}>
+        <TouchableOpacity style={UserProfileScreenStyles.addPhotoButton}>
           <Icon
             name="plus-circle"
             type="material-community"
@@ -21,8 +22,8 @@ const PersonalInfoScreen = () => {
             color="#252525"
           />
         </TouchableOpacity>
-        <Text style={styles.photoText}>Hello, Terrence</Text>
-        <Text style={styles.photoHint}>
+        <Text style={UserProfileScreenStyles.photoText}>Hello, Terrence</Text>
+        <Text style={UserProfileScreenStyles.photoHint}>
           Add or change a profile photo by pressing the plus button above.
         </Text>
       </View>
@@ -31,56 +32,4 @@ const PersonalInfoScreen = () => {
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 20,
-    backgroundColor: "#fff",
-  },
-  header: {
-    fontSize: 24,
-    fontWeight: "bold",
-    marginBottom: 20,
-  },
-  photoContainer: {
-    alignItems: "center",
-    marginBottom: 30,
-    padding: 20,
-  },
-  addPhotoButton: {
-    position: "absolute",
-    top: 20,
-    right: 120,
-  },
-  photoText: {
-    fontSize: 25,
-    fontWeight: "bold",
-    textAlign: "center",
-    marginVertical: 10,
-  },
-  photoHint: {
-    color: "#d3d3d3",
-    textAlign: "center",
-  },
-  infoContainer: {
-    marginTop: 20,
-  },
-  infoItem: {
-    flexDirection: "row",
-    alignItems: "center",
-    paddingVertical: 10,
-    borderBottomWidth: 1,
-    borderBottomColor: "#d3d3d3",
-  },
-  infoText: {
-    flex: 1,
-    fontSize: 16,
-    marginLeft: 10,
-  },
-  editText: {
-    color: "#252525",
-    fontSize: 16,
-  },
-});
-
-export default PersonalInfoScreen;
+export default UserProfileScreen;
